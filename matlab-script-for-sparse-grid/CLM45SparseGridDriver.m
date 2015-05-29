3bb103aa0e3660ae7f36d19ce5caeed3eb6b2f43
Gautam Bisht <gbisht@lbl.gov>
2015-05-28 23:55:08 -0700

    updates the message on how to run CLM45SparseGridDriver.m

--- matlab-script-for-sparse-grid/CLM45SparseGridDriver.m
+++ matlab-script-for-sparse-grid/CLM45SparseGridDriver.m
@@ -8,8 +8,14 @@
 % INPUT:
 %       fname = Configuration file name.
 %
-% EXAMPLE
-%  CLM45SparseGridDriver('')
+% EXAMPLE:
+%  # Download data
+%  mkdir clm-netcdf
+%  svn export https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata/lnd/clm2/surfdata_map/surfdata_1.9x2.5_simyr2000_c141219.nc clm-netcdf/surfdata_1.9x2.5_simyr2000_c141219.nc
+%  svn export https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata/share/domains/domain.clm/domain.lnd.fv1.9x2.5_USGS.110713.nc clm-netcdf/domain.lnd.fv1.9x2.5_USGS.110713.nc 
+%
+%  # Run matlab script
+%  CLM45SparseGridDriver('82x1_sparse_grid/82x1_sparse_grid.cfg')
 %
 % Gautam Bisht (gbisht@lbl.gov)
 % 05-28-2015
